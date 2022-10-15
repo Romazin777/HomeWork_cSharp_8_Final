379dc2fb8bb00436e43dd4689c7abb738c71f40f
Denis Romazin <romazin777@gmail.com>
2022-10-15 12:56:52 +0500

    Задача №60

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@
 // 9 5 3 2
 // 8 4 4 2
 
-
+// int[,] array = GetArray(5, 5, 0, 9);
+// PrintArray(array);
+// Console.WriteLine("В итоге получается вот такой массив: ");
 // PrintArray(OrderingRowsArray(array));
 
 
@@ -20,12 +22,53 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] array = GetArray(5, 5, 0, 9);
-PrintArray(array);
-Console.WriteLine($"Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: {MinSumValueRowsArray(array)} строка");
+// int[,] array = GetArray(5, 5, 0, 9);
+// PrintArray(array);
+// Console.WriteLine($"Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: {MinSumValueRowsArray(array)} строка");
 
 
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
 
+int[,,] array3D = GetOrderArray3D(2, 2, 2, 10);
+PrintArray3D(array3D);
+
+
+void PrintArray3D(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] GetOrderArray3D(int rows, int columns, int pages, int startValue)
+{
+    int[,,] result = new int[rows, columns, pages];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < pages; k++)
+            {
+                startValue++;
+                result[i, j, k] = startValue;
+            }
+        }
+    }
+    return result;
+}
 
 int MinSumValueRowsArray(int[,] array)
 {
@@ -48,7 +91,7 @@ int MinSumValueRowsArray(int[,] array)
             {
                 minSum = sum;
                 minRows = i;
-            }    
+            }
         }
         sum = 0;
     }
